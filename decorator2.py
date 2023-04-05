991890236ed4d996b2a71b6c57541299f96f8223
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-06 01:28:47 +0530

    added sub function

--- decorator2.py
+++ decorator2.py
@@ -1,15 +1,17 @@
 #greet is a decorator function.
 
-def greet(add):
+def greet(fx):
     def mfx():
         print("Good Morning Ayush")
-        add()
-        print("Have a nice Day")
+        fx()
+        print("Have a nice Day.")
+        print()
     return mfx()
-    
-
 
 @greet
 def add(x=2, y=7):
-    print(f"Sum of {x} and {y} is {x+y}")
-    
+    print(f"The addition of {x} and {y} is {x+y}")
+ 
+@greet   
+def sub(x=7, y=2):
+    print(f"The subraction of {x} and {y} is {x-y}")
