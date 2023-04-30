5e2b3dce94ad859534e5abc96b498777caaa4e5e
Ayush <ayushsilarpuria007@gmail.com>
2023-04-30 23:11:11 +0530

    Armstrong_num

--- GeeksforGeeks/BasicPrograms/6_Armstrong_num.py
+++ GeeksforGeeks/BasicPrograms/6_Armstrong_num.py
@@ -1,2 +1,17 @@
 # Program to check Armstrong Number
 
+# Method-1:
+
+n = int(input("Enter number: "))
+s = n 
+b = len(str(n))
+sum1 = 0
+
+while n != 0:
+    r = n%10
+    sum1 = sum1+(r**b)
+    n = n//10
+if s == sum1:
+    print("The given number", s, "is Armstrong Number.")
+else:
+    print("The given number", s, "is not Armstrong Number.")
