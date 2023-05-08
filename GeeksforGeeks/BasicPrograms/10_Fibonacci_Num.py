519f0df6753c6a1cb5b477b4feac460c994d388c
Ayush <ayushsilarpuria007@gmail.com>
2023-05-08 17:28:26 +0530

    Fibonacci Number Series

---
+++ GeeksforGeeks/BasicPrograms/10_Fibonacci_Num.py
@@ -0,0 +1,98 @@
+# Program for n-th Fibonacci number:
+
+def fibonacci(n):
+    if n <= 0:
+        print("INCORRECT INPUT") # First Fibonacci number is 0.
+    elif n == 1:
+        return 0 # Second Fibonacci number is 1.
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+num = int(input("Enter number: "))
+print(fibonacci(num))
+
+print()
+
+# Time_Complexity: O(2^n)
+# Space_Complexity: O(n)
+
+# Method-2:
+
+fibarray = [0,1]
+
+def fibonacci(n):
+    if n<0:
+        print("Incorrect Input")
+    elif n<= len(fibarray):
+        return fibarray[n-1]
+    else:
+        temp_fib = fibonacci(n-1) + fibonacci(n-2)
+        fibarray.append(temp_fib)
+        return temp_fib
+
+print(fibonacci(9))
+print(fibarray)
+
+print()
+
+# Time_Complexity: O(n)
+# Space_Complexity: O(n)
+
+fiblist = [0,1]
+# print(len(fiblist))
+def fib(n):
+    if n < 0:
+        print("Invalid input")
+    elif n<=len(fiblist):
+        return fiblist[n-1]
+    else:
+        new_fib = fib(n-1) + fib(n-2)
+        fiblist.append(new_fib)
+        return new_fib
+print(fib(9))
+print(fiblist)
+
+print()
+
+# Method-3:
+def fibo(n):
+    if n<=0:
+        return "Incorrect Output"
+    data = [0,1]
+    if n > 2:
+        for i in range(2,n):
+            datanow = data[i-1] + data[i-2]
+            data.append(datanow)
+    return data[n-1]
+    print(data)
+print(fibo(10))
+
+
+print()
+
+# Time_Complexity: O(n)
+# Space_Complexity: O(n)
+
+# Method-4:
+
+def fibonnaci(n):
+    a = 0
+    b =1
+    if n<= 0:
+        print("Incorrect Input")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2,n): 
+            c = a + b
+            a,b = b,c
+        return b
+    
+print(fibonacci(9))
+
+# Time_complexity: O(n)
+# Space_complexity: O(1)
